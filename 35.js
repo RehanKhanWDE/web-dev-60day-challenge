2ea0eaf87d8acce38d30891374250b7d2cafcdfb
Rehan Khan <rehankhan935055@gmail.com>
2026-08-01 21:16:52 +0530

    35.js

---
+++ 35.js
@@ -0,0 +1,24 @@
+function red()
+{
+    document.bgColor = "#FF0000"; // Red ka code
+}
+
+function yellow()
+{
+    document.bgColor = "#FFFF00"; // Yellow ka code
+}
+
+function blue()
+{
+    document.bgColor = "#0000FF"; // Blue ka code
+}
+
+function pink()
+{
+    document.bgColor = "#FFC0CB"; // Pink ka code
+}
+
+function white()
+{
+    document.bgColor = "white"; // Reset karne ke liye
+}
