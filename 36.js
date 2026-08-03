4161240185383af008147585e1c26c80935b38b0
Rehan Khan <rehankhan935055@gmail.com>
2026-08-03 22:09:38 +0530

    36.js

---
+++ 36.js
@@ -0,0 +1,10 @@
+function calc()
+{
+    var a = parseInt(form1.t1.value);
+    var b = parseInt(form1.t2.value);
+    
+    document.write("Addition = " + (a + b) + "<br>");
+    document.write("Multiplication = " + (a * b) + "<br>");
+    document.write("Division = " + (a / b) + "<br>");
+    document.write("subtraction = " + (a - b));
+}
